84827bf7d6e4ef68b2f39fae779a0855ae72d52e
Dariusz Okonski <darek200180@gmail.com>
2019-11-20 12:19:55 +0000

    display Lottery component at App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import Ball from './Ball';
+import Lottery from './Lottery';
 
 function App() {
   return (
     <div>
-      <Ball num={17} />
-      <Ball num={13} />
-      <Ball num={11} />
-      <Ball num={9} />
+      <Lottery />
+      <Lottery title='Mini Daily' maxNum={10} numBalls={4} />
     </div>
   );
 }
